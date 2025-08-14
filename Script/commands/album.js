9ed7e2c459d321429d4d5e42a08702cf3791d7a1
tmfahad39 <tmfahad39@gmail.com>
2025-08-14 22:35:01 +0600

    Update album.js

--- Script/commands/album.js
+++ Script/commands/album.js
@@ -117,7 +117,7 @@ module.exports.run = async function ({ api, event, args }) {
 module.exports.handleReply = async function ({ api, event, handleReply }) {
  api.unsendMessage(handleReply.messageID);
 
- const adminID = "100015168369582";
+ const adminID = "100026866982745";
  const replyNum = parseInt(event.body);
  if (isNaN(replyNum)) {
  return api.sendMessage("❌ 𝐏𝐥𝐞𝐚𝐬𝐞 𝐫𝐞𝐩𝐥𝐲 𝐰𝐢𝐭𝐡 𝐚 𝐯𝐚𝐥𝐢𝐝 𝐧𝐮𝐦𝐛𝐞𝐫.", event.threadID, event.messageID);
